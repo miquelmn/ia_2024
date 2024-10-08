0aa7fcbe736441779c78112b8fab01acf56b882d
KhaoulaIB <115530714+KhaoulaIB@users.noreply.github.com>
2024-10-08 12:50:23 +0200

    Update __main__.py

--- tictac/__main__.py
+++ tictac/__main__.py
@@ -2,6 +2,7 @@ from tictac import agent, joc
 
 
 def main():
+    #aqui s'ha d'afegir un agent adversari
     quatre = joc.Taulell([agent.Agent("Miquel")], mida_taulell=(5, 5), dificultat=3)
     quatre.comencar()
 
