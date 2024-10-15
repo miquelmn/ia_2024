de481d96739e61a2ceb898d6a9622dbec3725480
Miquel Miró Nicolau <miquel.miro@uib.cat>
2024-10-15 08:46:39 +0200

    New version of tic tac solution

---
+++ solucio/tictac_no_optim/__main__.py
@@ -0,0 +1,14 @@
+from tictac import agent, joc
+
+
+def main():
+    quatre = joc.Taulell(
+        [agent.Agent("Miquel"), agent.Agent("Francesc")],
+        mida_taulell=(3, 3),
+        dificultat=3,
+    )
+    quatre.comencar()
+
+
+if __name__ == "__main__":
+    main()
