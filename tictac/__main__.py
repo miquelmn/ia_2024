858f3a2c11c72bf567536ec0dcd6086313843866
Miquel Miró Nicolau <miquel.miro@uib.cat>
2024-10-15 09:07:17 +0200

    Solution of Tic Tac Toe

--- tictac/__main__.py
+++ tictac/__main__.py
@@ -1,8 +1,10 @@
-from tictac import agent, joc
+from tictac import joc
+from tictac import agent_s_no as agent
+# from tictac import agent_s_o as agent
 
 
 def main():
-    quatre = joc.Taulell([agent.Agent("Miquel")], mida_taulell=(5, 5), dificultat=3)
+    quatre = joc.Taulell([agent.Agent("Miquel"), agent.Agent("Miquel2")], mida_taulell=(3, 3), dificultat=3)
     quatre.comencar()
 
 
